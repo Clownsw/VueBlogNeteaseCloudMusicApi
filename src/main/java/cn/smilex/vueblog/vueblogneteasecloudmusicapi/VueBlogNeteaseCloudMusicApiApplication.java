c1e2bf9802c35844ce604a8f22b17c9600973f75
Clownsw <msmliexx1@gmail.com>
2022-09-11 17:52:51 +0800

    init project
    Signed-off-by: Clownsw <msmliexx1@gmail.com>

---
+++ src/main/java/cn/smilex/vueblog/vueblogneteasecloudmusicapi/VueBlogNeteaseCloudMusicApiApplication.java
@@ -0,0 +1,13 @@
+package cn.smilex.vueblog.vueblogneteasecloudmusicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBlogNeteaseCloudMusicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueBlogNeteaseCloudMusicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/smilex/vueblog/vueblogneteasecloudmusicapi/VueBlogNeteaseCloudMusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.smilex.vueblog.vueblogneteasecloudmusicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBlogNeteaseCloudMusicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
