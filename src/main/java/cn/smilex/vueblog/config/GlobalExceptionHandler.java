d1bf518f696d44315276317e872d9306000951d1
Smliexx <msmliexx1@gmail.com>
2022-09-21 00:01:24 +0800

    optimize code
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- src/main/java/cn/smilex/vueblog/config/GlobalExceptionHandler.java
+++ src/main/java/cn/smilex/vueblog/config/GlobalExceptionHandler.java
@@ -1,10 +1,11 @@
 package cn.smilex.vueblog.config;
 
 import cn.smilex.vueblog.model.Result;
+import cn.smilex.vueblog.util.CommonUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,12 +21,19 @@ import java.util.Arrays;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private CommonUtil commonUtil;
+
+    @Autowired
+    public void setCommonUtil(CommonUtil commonUtil) {
+        this.commonUtil = commonUtil;
+    }
+
     @SneakyThrows
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public String error(Exception e) {
         log.error(Arrays.toString(e.getStackTrace()));
-        return new ObjectMapper().writeValueAsString(Result.error("unknown error"));
+        return commonUtil.toJsonStr(Result.error("unknown error"));
     }
 
     @SneakyThrows
@@ -33,6 +41,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JsonProcessingException.class)
     public String error(JsonProcessingException e) {
         log.error(Arrays.toString(e.getStackTrace()));
-        return new ObjectMapper().writeValueAsString(Result.error("system inner error"));
+        return commonUtil.toJsonStr(Result.error("system inner error"));
     }
 }
